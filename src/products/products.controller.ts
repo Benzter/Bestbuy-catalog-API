ac7fddd56a14efc2c3e2da31be63e2bc78c541c8
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-22 12:51:58 +0530

    Add addProduct route handler to product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
 
     constructor(private productSerive: ProductsService) { }
+
+    @Post()
+    async addProduct(@Body("title") prodTitle: string,
+        @Body("price") price: number,
+        @Body("brand") brand: string,
+        @Body("image") imageUrl: string,
+    ) {
+        return await this.productSerive.insertProduct(prodTitle, price, brand, imageUrl);
+    }
+
     @Get()
     getAllProducts(): object {
         return { name: this.productSerive.text }
