cacce78502f1aa8af8d3e517965ecd95c92682fd
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-22 12:50:59 +0530

    Update productService to add product to database

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
+    constructor(@InjectModel("Product") private readonly productModel: Model<Product>) { }
     text = "Dhanushka";
+
+    async insertProduct(title: string, price: number, brand: string, imageUrl: string) {
+        const newProduct = new this.productModel({ title, price, brand, imageUrl })
+
+        const result = await newProduct.save();
+        return result;
+    }
 }
