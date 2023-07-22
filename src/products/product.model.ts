0f1af5a8c5f91b3f572e388dbece27d64aefc5f1
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2023-07-22 12:50:25 +0530

    Update Product model with product interface

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
 export const ProductSchema = new mongoose.Schema({
-    id: { type: String, required: true },
     title: { type: String, required: true },
     price: { type: Number, required: true },
     brand: { type: String, required: true },
     imageUrl: { type: String, required: true },
 });
+
+export interface Product {
+    title: string;
+    price: number;
+    brand: string;
+    imageUrl: string;
+}
